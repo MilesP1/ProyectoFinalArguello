a3fcdd29b994e9793d60b4e1e4d34091985aef56
unknown <arguellodario1@gmail.com>
2023-08-22 18:38:36 -0300

    html incompleto2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const respuestas = [
     { adivinanza: "Si me nombran, desaparezco. SOY EL...", respuesta: "silencio" },
     { adivinanza: "Cien hermanitos en una sola tabla, si nadie los toca ninguno habla. SOY EL...", respuesta: "piano" },
     { adivinanza: "Si soy joven, Joven quedo. Si soy viejo, viejo quedo. Tengo boca pero no hablo, tengo ojos pero no veo. SOY UN...", respuesta: "retrato" },
-    { adivinanza: "Juan tuvo 200 Paltas, Pedro va a tener 700 y Ana tiene 400. ¿Quién tiene mas Paltas?", respuesta: "ana"}
+    { adivinanza: "Juan tuvo 200 Paltas, Pedro va a tener 700 y Ana tiene 400. ¿Quién tiene mas Paltas?", respuesta: "ana"},
 ];
 
 // DOM para traer los elementos, aplicar para controlar las adivinanzas
@@ -18,38 +18,11 @@ const botonRespuestas = document.getElementById("btnRespuesta");
 const listaResultados = document.getElementById("resultadosLista");
 const loginBienv = document.getElementById("loginbienvenido");
 const botonlogin = document.getElementById("btnlogin");
+const botontemp = document.getElementById("iniciarTemp");
 
 function mostrarAdivinanza() {
-    const respuestaUsuario = respuestas00.value.toLowerCase();
-    const respuestaCorrecta = respuestas[adivinanzaIndex].respuesta;
-    
-    if (respuestaUsuario === respuestaCorrecta) {
-        resultados.push({ usuario: loginBienv.value, adivinanza: "Correcta" });
-        Swal.fire("Respuesta Correcta")
-    } else {
-        resultados.push({ usuario: loginBienv.value , adivinanza: "Incorrecta" });
-        Swal.fire("Respuesta Incorrecta")
-    }
     adivinanzas00.textContent = respuestas[adivinanzaIndex].adivinanza;
     respuestas00.value = '';
-}
-botonlogin.addEventListener("click", () =>{
-
-    Swal.fire(
-        "Gracias por Registrarte",
-        "Empecemos",
-        mostrarAdivinanza()
-    )
-})
-
-function verificarRespuesta() {
-    adivinanzaIndex++;
-    if (adivinanzaIndex < respuestas.length) {
-        mostrarAdivinanza();
-    } else {
-        Swal.fire("Game Over")
-        mostrarResultados();
-    }
 }
 
 function mostrarResultados() {
@@ -62,5 +35,34 @@ function mostrarResultados() {
     localStorage.setItem("resultados", JSON.stringify(resultados));
 }
 
+function verificarRespuesta() {
+    const respuestaUsuario = respuestas00.value.toLowerCase();
+    const respuestaCorrecta = respuestas[adivinanzaIndex].respuesta;
+    if (respuestaUsuario === respuestaCorrecta) {
+        resultados.push({ usuario: loginBienv.value.toLowerCase(), adivinanza: "Correcta" });
+        Swal.fire("Respuesta Correcta")
+    } else {
+        resultados.push({ usuario: loginBienv.value.toLowerCase(), adivinanza: "Incorrecta" });
+        Swal.fire("Respuesta Incorrecta")
+    }
+  
+adivinanzaIndex++;
+if (adivinanzaIndex < respuestas.length) {
+    mostrarAdivinanza();
+} else {
+    Swal.fire("Game Over");
+}
+}
+
+botontemp.addEventListener("click", () =>{
+    mostrarResultados();
+})
+
+botonlogin.addEventListener("click", () =>{
+    Swal.fire(
+        "Gracias por Registrarte",
+        mostrarAdivinanza(),
+    )
+})
 botonRespuestas.addEventListener("click", verificarRespuesta);
 console.log(resultados);
