828e4d1482b43cc487d2fb57371b38b0899e8f2a
unknown <arguellodario1@gmail.com>
2023-08-29 02:22:14 -0300

    Finalizar detalles

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 /*Codigo Adivinanzas*/
 let i = 0;
-
 // Array con objetos adentro (Adivinanza y respuesta)
 const respuestas = [
-    { adivinanza: "Cuando paso el Hierro se oxida, el acero se rompe y la carne se pudre. SOY EL...", respuesta: "tiempo" },
-    { adivinanza: "Si me nombran, desaparezco. SOY EL...", respuesta: "silencio" },
-    { adivinanza: "Cien hermanitos en una sola tabla, si nadie los toca ninguno habla. SOY EL...", respuesta: "piano" },
-    { adivinanza: "Si soy joven, Joven quedo. Si soy viejo, viejo quedo. Tengo boca pero no hablo, tengo ojos pero no veo. SOY UN...", respuesta: "retrato" },
-    { adivinanza: "Juan tuvo 200 Paltas, Pedro va a tener 700 y Ana tiene 400. ¿Quién tiene mas Paltas?", respuesta: "ana"},
+    { adivinanza: "¿Qué Pokemon aparece en el primer episodio luchando contra Nidorino?", respuesta: "gengar"},
+    { adivinanza: "¿A qué Pokemon abandonan bajo la lluvia y es rescatado por Ash?", respuesta: "charmander" },
+    { adivinanza: "El Equipo Rocket roba un Pokemon en Pueblo Newpark. ¿Cuál es?", respuesta: "totodile" },
+    { adivinanza: "¿Quién vence a Hariyama en el Gimnasio Dewford?", respuesta: "treecko" },
+    { adivinanza: "¿Cuál es el primer Pokemon de James en la Region Hoenn?", respuesta: "cacnea"},
 ];
-
 // DOM para traer los elementos, aplicar para controlar las adivinanzas
 const adivinanzas00 = document.getElementById("adivtext");
 const respuestas00 = document.getElementById("respAdiv");
@@ -17,12 +15,29 @@ const botonRespuestas = document.getElementById("btnRespuesta");
 const listaResultados = document.getElementById("resultadosLista");
 const loginBienv = document.getElementById("loginbienvenido");
 const botonlogin = document.getElementById("btnlogin");
-const botontemp = document.getElementById("iniciarTemp");
+const buttonResultados = document.getElementById("btnResultados");
+const botonpista = document.getElementById("btnpistas01");
+const gengarDiv = document.getElementById("gengarDiv");
+const charmanderDiv = document.getElementById("gengarDiv");
 const usuarioNuevo = {usuario:"",respuestas: {correctas:0, incorrectas:0}};
-
+let renderizada = false;
+let secondsRemaining = 60;
+////temporizador
+function updateTimer() {
+  const timerDisplay = document.getElementById("timer");
+  timerDisplay.textContent = secondsRemaining + " segundos";
+  if (secondsRemaining <= 0) {
+    clearInterval(timerInterval);
+    timerDisplay.textContent = "Tiempo agotado";
+  }
+  secondsRemaining--;
+}
+// Actualizar las adivinanzas.
 function mostrarAdivinanza() {
-    adivinanzas00.textContent = respuestas[i].adivinanza;
-    respuestas00.value = '';
+  adivinanzas00.textContent = respuestas[i].adivinanza;
+  respuestas00.value = '';
+  updateTimer();
+  const timerInterval = setInterval(updateTimer, 1000);
 }
 
 function mostrarResultados() {
@@ -31,69 +46,122 @@ function mostrarResultados() {
     if (resultadoLocal) {
       resultadoLocal.forEach((resultado) => {
         const li = document.createElement("li");
-        li.textContent = `${resultado.usuario} -> Respuestas Correctas: ${resultado.correctas} Respuestas Incorrectas: ${resultado.incorrectas}`;
+        li.textContent = `${resultado.usuario} -Respuestas | Correctas: ${resultado.correctas} Incorrectas: ${resultado.incorrectas}`;
         listaResultados.appendChild(li);
       });
     } else {
       listaResultados.innerHTML = "No hay resultados para mostrar";
     }
   }
-
-function verificarRespuesta() {
-    const respuestaUsuario = respuestas00.value.toLowerCase();
-    const respuestaCorrecta = respuestas[i].respuesta;
-    if (respuestaUsuario === respuestaCorrecta) {
-      const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
-      resultadoLocal[resultadoLocal.length - 1].correctas += 1;
-      localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
-      Swal.fire("Respuesta Correcta");
-    } else {
-      const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
-      console.log(resultadoLocal);
-      resultadoLocal[resultadoLocal.length - 1].incorrectas += 1;
-      localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
-      Swal.fire("Respuesta Incorrecta");
-    }
   
-    i++;
-    if (i < 5) {
-      mostrarAdivinanza();
-    } else {
-      Swal.fire("Game Over");
-      i = 0;
-    }
+function verificarRespuesta() {
+  const respuestaUsuario = respuestas00.value.toLowerCase();
+  const respuestaCorrecta = respuestas[i].respuesta;
+  if (respuestaUsuario === respuestaCorrecta) {
+    const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
+    resultadoLocal[resultadoLocal.length - 1].correctas += 1;
+    localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
+    Swal.fire("Respuesta Correcta");
+  } else {
+    const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
+    console.log(resultadoLocal);
+    resultadoLocal[resultadoLocal.length - 1].incorrectas += 1;
+    localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
+    Swal.fire("Respuesta Incorrecta");
   }
-
-botontemp.addEventListener("click", () =>{
-    mostrarResultados();
-})
+  i++;
+  if (i < 5) {
+    mostrarAdivinanza();
+  } else {
+    Swal.fire("Game Over");
+  }
+}
 
 botonlogin.addEventListener("click", () => {
-    if (loginBienv.value != "") {
-      const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
-      if (resultadoLocal) {
-        resultadoLocal.push({
-          usuario: loginBienv.value,
-          correctas: 0,
-          incorrectas: 0,
-        });
-        localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
-      } else {
-        const resultadoLocal = [
-          {
-            usuario: loginBienv.value,
-            correctas: 0,
-            incorrectas: 0,
-          },
-        ];
-        localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
-      }
-  
-      Swal.fire("Gracias por Registrarte", mostrarAdivinanza());
+  if (loginBienv.value != "") {
+    const resultadoLocal = JSON.parse(localStorage.getItem("resultados"));
+    if (resultadoLocal) {
+      resultadoLocal.push({
+        usuario: loginBienv.value,
+        correctas: 0,
+        incorrectas: 0,
+      });
+      localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
     } else {
-      Swal.fire("coloque su nombre");
-      mostrarAdivinanza()
+      const resultadoLocal = [
+        {usuario: loginBienv.value, correctas: 0, incorrectas: 0,},
+      ];
+      localStorage.setItem("resultados", JSON.stringify(resultadoLocal));
+    }
+    Swal.fire("Gracias por Registrarte", mostrarAdivinanza());
+  } else {
+      Swal.fire("Ingrese su nombre");
     } 
   });
-botonRespuestas.addEventListener("click", verificarRespuesta);
-console.log(resultados);
+
+buttonResultados.addEventListener("click", () =>{
+    mostrarResultados();
+})
+botonRespuestas.addEventListener("click", verificarRespuesta, updateTimer);
+
+//Utilizando API en function (Proyecto Final)
+function callPokemon() {
+  let apiData = [];
+  let charmander = "https://pokeapi.co/api/v2/pokemon/charmander";
+  let gengar = "https://pokeapi.co/api/v2/pokemon/gengar";
+  let cacnea ="https://pokeapi.co/api/v2/pokemon/cacnea";
+  let treecko ="https://pokeapi.co/api/v2/pokemon/treecko";
+  let totodile ="https://pokeapi.co/api/v2/pokemon/totodile";
+
+  fetch(cacnea)
+  .then((response) => response.json())
+  .then((data) => {
+    const cacneaData = [data.sprites.front_default, data.name];
+    apiData.push(cacneaData);
+  })
+  .catch(() => console.error(exploto));
+
+  fetch(totodile)
+  .then((response) => response.json())
+  .then((data) => {
+    const totodileData = [data.sprites.front_default, data.name];
+    apiData.push(totodileData);
+  })
+  .catch(() => console.error(exploto));
+
+  fetch(treecko)
+  .then((response) => response.json())
+  .then((data) => {
+    const treeckoData = [data.sprites.front_default, data.name];
+    apiData.push(treeckoData);
+  })
+  .catch(() => console.error(exploto));
+
+  fetch(charmander)
+    .then((response) => response.json())
+    .then((data) => {
+      const charmanderData = [data.sprites.front_default, data.name];
+      apiData.push(charmanderData);
+    })
+    .catch(() => console.error(exploto));
+
+  fetch(gengar) //
+    .then((response) => response.json())
+    .then((data) => {
+      const gengarData = [data.sprites.front_default, data.name];
+      apiData.push(gengarData);
+
+      if (apiData.length > 0 && !renderizada) {
+        renderizada = true;
+
+        apiData.map((p) => {
+          const pista = document.createElement("div");
+          pista.innerHTML = `<div class="card"><img src="${p[0]}"> <h3>${p[1]}</h3></div>`;
+          gengarDiv.appendChild(pista);
+        });
+      }
+    })
+    .catch(() => console.error(exploto));
+}
+
+botonpista.addEventListener("click", callPokemon);
